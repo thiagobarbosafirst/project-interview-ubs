d3682be696f52080ef90efd47b19e1b16a76634e
Thiago Matias <tmbthiagobarbosa@gmail.com>
2021-04-21 15:54:58 -0300

    nova estrutura do databaseWriter

--- projectinterviewubs/src/main/java/com/ubs/projectinterviewubs/writer/database/DataBaseItemProductWriterConfig.java
+++ projectinterviewubs/src/main/java/com/ubs/projectinterviewubs/writer/database/DataBaseItemProductWriterConfig.java
@@ -1,7 +1,12 @@
-package com.ubs.projectinterviewubs.writer;
+package com.ubs.projectinterviewubs.writer.database;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -18,8 +23,27 @@ public class DataBaseItemProductWriterConfig {
 			@Qualifier("appDataSource") DataSource dataSource) {
 		return new JdbcBatchItemWriterBuilder<ProductItem>()
 				.dataSource(dataSource)
-				.sql("INSERT INTO product_item (product, quantity, price, type, industry, origin) VALUES (:product, :quantity, :price, :type, :industry, :origin)")
-				.beanMapped()
+				.sql("INSERT INTO product_item (product, quantity, price, type, industry, origin) VALUES (?, ?, ?, ?, ?, ?)")
+				.itemPreparedStatementSetter(itemPrepararedStatementSetter())
 				.build();
+	}
+
+	private ItemPreparedStatementSetter<ProductItem> itemPrepararedStatementSetter() {
+		
+		return new ItemPreparedStatementSetter<ProductItem>() {
+
+			@Override
+			public void setValues(ProductItem prod, PreparedStatement ps) throws SQLException {
+				ps.setString(1, prod.getProduct());
+				ps.setInt(2, prod.getQuantity());
+				ps.setBigDecimal(3, new BigDecimal(prod.getPrice().substring(1)));
+				ps.setString(4, prod.getType());
+				ps.setString(5, prod.getIndustry());
+				ps.setString(6, prod.getOrigin());
+				
+				
+			}
+			
+		};
 	}
 }
